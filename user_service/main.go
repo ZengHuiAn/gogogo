97d4bf1be1c90998ad4f3a9c6c448753652d45b8
AnZengHui <18530895881@163.com>
2019-07-02 13:36:47 +0800

    module

---
+++ user_service/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/micro/go-micro"
+	transport "github.com/micro/go-plugins/transport"
+	proto "github.com/snake/test/greeter"
+)
+
+type Transceiver struct{}
+
+func (g *Transceiver) Transmission(ctx context.Context, req *proto.SendRequest, rsp *proto.SendResponse) error {
+	rsp.MessageId = 1
+	rsp.Body = req.Body
+	rsp.Type = proto.MessageType_Notify
+
+	fmt.Println(rsp.GetBody())
+
+	return nil
+}
+
+func main() {
+	// Create a new service. Optionally include some options here.
+	service := micro.NewService(
+		micro.Name("tcp"),
+	)
+	transport.
+		// Init will parse the command line flags.
+		service.Init()
+
+	// Register handler
+	_ = proto.RegisterTransceiverHandler(service.Server(), new(Transceiver))
+
+	// Run the server
+	if err := service.Run(); err != nil {
+		fmt.Println(err)
+	}
+}
